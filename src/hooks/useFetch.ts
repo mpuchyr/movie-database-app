144a508abe11c27a15dcca81ac190914f6ee8b17
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-08-10 15:18:57 -0500

    Add starting code to hooks/useFetch.ts

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,6 +1,18 @@
 import { useState } from 'react';
 
 const useFetch = () => {
+    const [data, setData] = useState(null)
+    const url = 'https://api.themoviedb.org'
+    const token = process.env.TMDB_ACCESS_TOKEN
+    
+    try {
+        fetch(`${url}/3/disover/movie?api_key=${token}`)
+        .then(res => res.json())
+        .then(data => setData(data))
+    } catch {
+        
+    }
+
 }
 
 export default useFetch
