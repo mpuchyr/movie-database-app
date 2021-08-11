1c22d8bb9daaa8337ce6529735721d5da001d883
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-08-11 15:57:05 -0500

    Add initial useEffect in components/MainWindow.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import * as dotenv from 'dotenv';
+import MainWindow from './components/MainWindow';
 
 function App() {
+
   return (
     <div className="App">
       Hello world
+      <MainWindow />
     </div>
   );
 }
--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -1,4 +1,26 @@
+import { useState, useEffect } from 'react';
+import * as dotenv from 'dotenv';
+import moment from 'moment';
+
 const MainWindow = () => {
+    const [upcoming, setUpcoming] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
+    const [date, setDate] = useState(moment().format("YYYY-MM-DD"))
+
+    const token = "203505d9985d3a84ee017420d7ddd358"
+
+    useEffect(() => {
+      try {
+        fetch(`https://api.themoviedb.org/3/movie/550?api_key=${token}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+      } catch {
+        setError('Something went wrong, please try again later')
+      }
+      setLoading(false)
+    }, [])
+
     return (
         <div>
             Main Window Placeholder
