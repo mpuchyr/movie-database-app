ee2a823d734688e3bb34879ec5e7c923247a29ed
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-08-13 16:50:18 -0500

    Modify hooks/useFetch to not use process.env until a better solution can be found

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,14 +3,14 @@ import { useState } from 'react';
 const useFetch = () => {
     const [data, setData] = useState(null)
     const url = 'https://api.themoviedb.org'
-    const token = process.env.TMDB_ACCESS_TOKEN
+    const token = "203505d9985d3a84ee017420d7ddd358"
     
     try {
         fetch(`${url}/3/disover/movie?api_key=${token}`)
         .then(res => res.json())
-        .then(data => setData(data))
+        .then(data => console.log(data))
     } catch {
-        
+        console.log('There was an error fetching data')
     }
 
 }
